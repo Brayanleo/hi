a484cea3f2cd38d205699af3842245af351dadca
Estudiante <Estudiante@BE454S402C46ING.unal.loc>
2019-04-02 08:14:48 -0500

    el brayan sin for

---
+++ src/hi/HI.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hi;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class HI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a;
+        a=2;
+        
+        char b[]={'E','l',' ','B','r','a','y','a','n'};
+        
+        System.out.println("Hola mundo");
+        System.out.println(a);
+        System.out.println(b[0]);
+        
+    }
+           
+                
+                
+}
